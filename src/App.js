2903e966a840fe17cbc1ab970a525bd8dec02a00
kiranfrnds <kiran.minpro@gmail.com>
2022-06-11 19:31:57 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { getPosts, getSinglePost } from "./redux/features/postSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  const [id, setId] = useState();
+  const posts = useSelector((state) => state.posts.posts);
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
+  const setActiveId = (id) => {
+    setId(id);
+    dispatch(getSinglePost(id));
+    console.log(id);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Posts</h1>
+      <div>
+        {posts.loading ? (
+          <div>Loading...</div>
+        ) : (
+          <div>
+            <h1>{posts.id}</h1>
+            <h1>{posts.name}</h1>
+            <h1>{posts.email}</h1>
+          </div>
+        )}
+      </div>
+
+      <ul>
+        {posts &&
+          posts.length &&
+          posts?.map((post) => (
+            <li key={post.id} onClick={(id) => setActiveId(post.id)}>
+              <h1>{post.id}</h1>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
---
+++ src/redux/features/postSlice.js
@@ -0,0 +1,57 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const getPosts = createAsyncThunk("posts/getPosts", async () => {
+  const response = await fetch("https://reqres.in/api/users?page=1");
+  const posts = await response.json();
+  return posts.data;
+});
+
+export const getSinglePost = createAsyncThunk("posts/getPost", async (id) => {
+  const response = await fetch(`https://reqres.in/api/users/${id}`);
+  const post = await response.json();
+  return post.data;
+});
+
+export const postSlice = createSlice({
+  name: "posts",
+  initialState: {
+    posts: [],
+    loading: false,
+    error: null,
+  },
+  reducers: {
+    getPost: (state, action) => {
+      state.posts = action.payload;
+    },
+    getSinglePost: (state, action) => {
+      state.posts = action.payload;
+    },
+  },
+  extraReducers: {
+    [getPosts.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [getPosts.fulfilled]: (state, action) => {
+      state.posts = action.payload;
+      state.loading = false;
+    },
+    [getPosts.rejected]: (state, action) => {
+      state.error = action.error;
+      state.loading = false;
+    },
+    [getSinglePost.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [getSinglePost.fulfilled]: (state, action) => {
+      state.posts = action.payload;
+      state.loading = false;
+    },
+    [getSinglePost.rejected]: (state, action) => {
+      state.error = action.error;
+      state.loading = false;
+    },
+  },
+});
+
+export default postSlice.reducer;
+export const { getPost, getSingleSlot } = postSlice.actions;
